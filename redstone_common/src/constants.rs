3811a22a737dffb6d0fa472ec462c08d84ba691f
hammsvietro <49427492+hammsvietro@users.noreply.github.com>
2022-11-18 11:05:52 -0300

    Update constants.rs

--- redstone_common/src/constants.rs
+++ redstone_common/src/constants.rs
@@ -6,4 +6,4 @@ pub const IPC_BUFFER_SIZE: usize = 16384;
 #[cfg(target_os = "macos")]
 pub const IPC_BUFFER_SIZE: usize = 8192;
 
-pub const TCP_FILE_CHUNK_SIZE: usize = 1024 * 1024 * 14; // 50MB
+pub const TCP_FILE_CHUNK_SIZE: usize = 1024 * 1024 * 14; // 14MB
