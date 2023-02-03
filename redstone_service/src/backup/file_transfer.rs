3d2ec57ee677f91a2b15b142bc8a8b8f35e9123c
hammsvietro <hammsvietro@gmail.com>
2023-02-03 08:20:41 -0300

    Refactors file_transfer functions and fixes fs_tree tests

--- redstone_service/src/backup/file_transfer.rs
+++ redstone_service/src/backup/file_transfer.rs
@@ -1,4 +1,7 @@
-use std::path::PathBuf;
+use std::{
+    borrow::BorrowMut,
+    path::{Path, PathBuf},
+};
 
 use redstone_common::{
     constants::TCP_FILE_CHUNK_SIZE,
@@ -29,58 +32,17 @@ pub async fn send_files(
     let mut stream = BufReader::new(stream);
     let mut bytes_sent: u64 = 0;
     for file in files {
-        println!("Uploading {} file", file.path);
-        let mut retry_count: u8 = 0;
-        loop {
-            let mut file_upload_message =
-                FileUploadMessageFactory::new(upload_token, file, root_folder.clone());
-            while file_upload_message.has_data_to_fetch() {
-                let packet = file_upload_message.get_tcp_payload()?;
-                send_message(&mut stream, &packet).await?;
-                bytes_sent += file_upload_message.last_chunk_size as u64;
-                progress_emitter.send(bytes_sent)?;
-                let response: TcpMessageResponse<()> = receive_message(&mut stream).await?;
-                if response.status != TcpMessageResponseStatus::Ok {
-                    // TODO: send abort message
-                    let error = format!(
-                        "Error commiting backup transaction.\nServer responded: {}",
-                        response.reason.unwrap()
-                    );
-                    return Err(RedstoneError::BaseError(error));
-                }
-            }
-            let check_file_message =
-                CheckFileMessageFactory::new(upload_token, &file.id).get_tcp_payload()?;
-            println!("Verifying checksum");
-            send_message(&mut stream, &check_file_message).await?;
-            let response: TcpMessageResponse<()> = receive_message(&mut stream).await?;
-            if response.status == TcpMessageResponseStatus::Ok {
-                println!("Checksum matches");
-                println!("{} upload complete\n", file.path);
-                break;
-            } else {
-                retry_count += 1;
-                if retry_count > 4 {
-                    return Err(RedstoneError::BaseError(format!(
-                        "File upload retry count exceeded.\nServer returned: {:?}",
-                        response.reason.unwrap()
-                    )));
-                }
-            }
-        }
+        send_file(
+            &mut stream,
+            file,
+            upload_token,
+            &root_folder,
+            &progress_emitter,
+            &mut bytes_sent,
+        )
+        .await?;
     }
-    let commit_payload = CommitMessageFactory::new(upload_token.clone()).get_tcp_payload()?;
-    println!("Sending commit msg!");
-    send_message(&mut stream, &commit_payload).await?;
-    let response: TcpMessageResponse<()> = receive_message(&mut stream).await?;
-    if response.status != TcpMessageResponseStatus::Ok {
-        let error = format!(
-            "Error commiting backup transaction.\nServer responded: {}",
-            response.reason.unwrap()
-        );
-        return Err(RedstoneError::BaseError(error));
-    }
-    Ok(())
+    send_commit_msg(&mut stream, upload_token).await
 }
 
 pub async fn download_files(
@@ -92,37 +54,14 @@ pub async fn download_files(
     let mut stream = BufReader::new(stream);
     let mut _bytes_received: u64 = 0;
     for file in files {
-        let mut path = root.clone();
-        path.push(file.path.clone());
-        if path.is_file() {
-            tokio::fs::OpenOptions::new()
-                .write(true)
-                .truncate(true)
-                .open(&path)
-                .await?;
-        } else if let Some(prefix) = path.parent() {
-            tokio::fs::create_dir_all(prefix).await?;
-        }
-        let mut factory = DownloadChunkMessageFactory::new(download_token.clone(), file.id.clone());
-        loop {
-            let packet = factory.get_tcp_payload()?;
-            send_message(&mut stream, &packet).await?;
-            let response: TcpMessageResponse<Vec<u8>> = receive_message(&mut stream).await?;
-            if response.data.is_none() {
-                break;
-            }
-            let data = response.data.unwrap();
-            _bytes_received += data.len() as u64;
-            let mut file = tokio::fs::OpenOptions::new()
-                .append(true)
-                .create(true)
-                .open(&path)
-                .await?;
-            file.write_all(&data).await?;
-            if data.len() == TCP_FILE_CHUNK_SIZE {
-                break;
-            }
-        }
+        download_file(
+            &mut stream,
+            file,
+            &root,
+            download_token.clone(),
+            &mut _bytes_received,
+        )
+        .await?;
         println!("downloaded {}", file.path);
     }
     let packet = FinishDownloadMessageFactory::new(download_token.to_string()).get_tcp_payload()?;
@@ -136,5 +75,110 @@ pub async fn download_files(
         );
         return Err(RedstoneError::BaseError(error));
     }
+    Ok(())
+}
+
+async fn send_file(
+    stream: &mut BufReader<TcpStream>,
+    file: &RSFile,
+    upload_token: &String,
+    root_folder: &Path,
+    progress_emitter: &UnboundedSender<u64>,
+    bytes_sent: &mut u64,
+) -> Result<()> {
+    println!("Uploading {} file", file.path);
+    let mut retry_count: u8 = 0;
+    loop {
+        let mut file_upload_message =
+            FileUploadMessageFactory::new(upload_token, file, root_folder.to_path_buf());
+        while file_upload_message.has_data_to_fetch() {
+            let packet = file_upload_message.get_tcp_payload()?;
+            send_message(stream.borrow_mut(), &packet).await?;
+            *bytes_sent += file_upload_message.last_chunk_size as u64;
+            progress_emitter.send(*bytes_sent)?;
+            let response: TcpMessageResponse<()> = receive_message(stream.borrow_mut()).await?;
+            if response.status != TcpMessageResponseStatus::Ok {
+                // TODO: send abort message
+                let error = format!(
+                    "Error commiting backup transaction.\nServer responded: {}",
+                    response.reason.unwrap()
+                );
+                return Err(RedstoneError::BaseError(error));
+            }
+        }
+        let check_file_message =
+            CheckFileMessageFactory::new(upload_token, &file.id).get_tcp_payload()?;
+        send_message(stream.borrow_mut(), &check_file_message).await?;
+        let response: TcpMessageResponse<()> = receive_message(stream.borrow_mut()).await?;
+        if response.status == TcpMessageResponseStatus::Ok {
+            println!("{} upload complete\n", file.path);
+            break;
+        } else {
+            retry_count += 1;
+            if retry_count > 4 {
+                return Err(RedstoneError::BaseError(format!(
+                    "File upload retry count exceeded.\nServer returned: {:?}",
+                    response.reason.unwrap()
+                )));
+            }
+        }
+    }
+    Ok(())
+}
+
+async fn send_commit_msg(stream: &mut BufReader<TcpStream>, upload_token: &str) -> Result<()> {
+    let commit_payload = CommitMessageFactory::new(upload_token.to_owned()).get_tcp_payload()?;
+    println!("Sending commit msg!");
+    send_message(stream.borrow_mut(), &commit_payload).await?;
+    let response: TcpMessageResponse<()> = receive_message(stream.borrow_mut()).await?;
+    if response.status != TcpMessageResponseStatus::Ok {
+        let error = format!(
+            "Error commiting backup transaction.\nServer responded: {}",
+            response.reason.unwrap()
+        );
+        return Err(RedstoneError::BaseError(error));
+    }
+    Ok(())
+}
+
+async fn download_file(
+    stream: &mut BufReader<TcpStream>,
+    file: &RSFile,
+    root: &Path,
+    download_token: String,
+    _bytes_received: &mut u64,
+) -> Result<()> {
+    let mut path = root.to_path_buf();
+    path.push(file.path.clone());
+    if path.is_file() {
+        tokio::fs::OpenOptions::new()
+            .write(true)
+            .truncate(true)
+            .open(&path)
+            .await?;
+    } else if let Some(prefix) = path.parent() {
+        tokio::fs::create_dir_all(prefix).await?;
+    }
+    let mut factory = DownloadChunkMessageFactory::new(download_token.clone(), file.id.clone());
+    loop {
+        let packet = factory.get_tcp_payload()?;
+        send_message(stream.borrow_mut(), &packet).await?;
+        let response: TcpMessageResponse<Vec<u8>> = receive_message(stream.borrow_mut()).await?;
+        if response.data.is_none() {
+            break;
+        }
+        let data = response.data.unwrap();
+        *_bytes_received += data.len() as u64;
+        let mut file = tokio::fs::OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open(&path)
+            .await?;
+        file.write_all(&data).await?;
+        if data.len() == TCP_FILE_CHUNK_SIZE {
+            break;
+        }
+    }
+    println!("downloaded {}", file.path);
     Ok(())
 }
